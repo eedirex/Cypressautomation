b128321e251d1251c8aa225ee77d8e9f747d720a
Idris Aliyu <idrisaliyu2010@gmail.com>
2022-07-11 09:24:17 +0100

    Testing updates

--- cypress/integration/DCIR/db.spec.js
+++ cypress/integration/DCIR/db.spec.js
@@ -2,7 +2,7 @@
 
 describe('Example to Demonstrate SQL Database Testing in Cypress', () => {
 
-    it('Verify that there is only one row where the city is Espoo', function () {
+    it('Verify the number of rows in the db', function () {
         cy.task('queryDb', `SELECT COUNT(*) as "rowCount" FROM dcir_db.backoffice_transaction_records`).then((result) => {
             console.log(result)
             expect(result[0].rowCount).to.equal(2536)
